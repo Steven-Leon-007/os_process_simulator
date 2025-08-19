e532a8f611592acda38060fae06075023856a37a
Steven-Leon-007 <estivlinyor@gmail.com>
2025-08-19 11:35:25 -0500

    First commit - Instances and dependencies

---
+++ src/components/process_node/ProcessNode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProcessNode = () => {
+  return (
+    <div>ProcessNode</div>
+  )
+}
+
+export default ProcessNode
---
+++ src/components/state-diagram/StateDiagram.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StateDiagram = () => {
+  return (
+    <div>StateDiagram</div>
+  )
+}
+
+export default StateDiagram
